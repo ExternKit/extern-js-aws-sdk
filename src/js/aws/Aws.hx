2efc1eff4e67e1388c7ab6f54a18f857ca3138b2
Fabien Antoine <fab.antoine@gmail.com>
2016-05-04 14:37:54 +0200

    Aws.config should be writable

--- src/js/aws/Aws.hx
+++ src/js/aws/Aws.hx
@@ -4,7 +4,7 @@ extern class Aws implements js.extern.Extern<'aws-sdk'>
 {
     public static var VERSION(default, never) : String;
 
-    public static var config(default, never) : Config;
+    public static var config(default, default) : Config;
 
     public static var events(default, never) : SequentialExecutor;
 }
